cf600b37f66cb83767de8debae19f38d18719924
kenta-elys <kenta@elys.network>
2023-10-03 07:25:53 +0000

    chore: updated schema

--- src/bin/schema.rs
+++ src/bin/schema.rs
@@ -1,5 +1,5 @@
 use cosmwasm_schema::write_api;
-use trade_shield_contract::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use financial_snapshot_contract::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
