6ce643c185c5d75c8bfb62310254a1506a849464
kenta-elys <kenta@elys.network>
2023-12-08 17:35:51 +0000

    chore: define usdc price resp

--- src/action/query/earn/get_usdc_price.rs
+++ src/action/query/earn/get_usdc_price.rs
@@ -2,10 +2,14 @@ use super::*;
 use cosmwasm_std::{Decimal, Uint128};
 use crate::bindings::{query::ElysQuery, querier::ElysQuerier};
 use crate::types::ElysDenom;
+use crate::msg::query_resp::earn::GetUsdcPriceResp;
 
-pub fn get_usdc_price(deps: Deps<ElysQuery>) -> Result<Decimal, ContractError> {
+pub fn get_usdc_price(deps: Deps<ElysQuery>) -> Result<GetUsdcPriceResp, ContractError> {
     let querier = ElysQuerier::new(&deps.querier);
     let usdc_oracle_price = querier.get_oracle_price(ElysDenom::USDC.as_str().to_string(), "".to_string(), 0)?;
     let usdc_usd_price = usdc_oracle_price.price.price.checked_div(Decimal::from_atomics(Uint128::new(1000000), 0).unwrap()).unwrap();
-    Ok(usdc_usd_price)
+    let resp = GetUsdcPriceResp {
+        price: usdc_usd_price,
+    };
+    Ok(resp)
 }
