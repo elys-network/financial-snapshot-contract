a23be47a24492f72589528fb28fae2e579009d66
Cosmic Vagabond <121588426+cosmic-vagabond@users.noreply.github.com>
2023-12-21 05:49:44 +0100

    ci: fix migrate (#21)

--- src/entry_point/migrate.rs
+++ src/entry_point/migrate.rs
@@ -1,6 +1,10 @@
 use cosmwasm_std::Empty;
+use cosmwasm_std::{
+    entry_point,
+};
 
 use super::*;
+use elys_bindings::*;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(_deps: DepsMut<ElysQuery>, _env: Env, _msg: Empty) -> StdResult<Response<ElysMsg>> {
